87ea29c5ecd718b8fa40fbcf0a731b14b4f16909
shishaosong <41780911+shishaosong@users.noreply.github.com>
2026-05-06 00:14:01 +0800

    Update index.js

--- mail-vue/src/i18n/index.js
+++ mail-vue/src/i18n/index.js
@@ -2,16 +2,16 @@ import { createI18n } from 'vue-i18n';
 import en from './en.js'
 import zh from './zh.js'
 
-// 优先从浏览器缓存读取语言设置，如果没有则默认为中文 'zh'
-const savedLanguage = localStorage.getItem('cloud_mail_lang') || 'zh';
+// 默认读取为 zh-cn
+const savedLanguage = localStorage.getItem('cloud_mail_lang') || 'zh-cn';
 
 const i18n = createI18n({
     legacy: false,
     locale: savedLanguage, 
     fallbackLocale: 'en',
     messages: {
-        zh,
-        en
+        'zh-cn': zh,   // <--- 核心修改：这里必须是 'zh-cn'，系统才认识
+        'en': en
     },
 });
 
